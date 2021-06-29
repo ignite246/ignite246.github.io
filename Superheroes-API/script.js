4b5b5f4f81d8cd17f7eeaa26578c162371c59bd6
Rahuk Kumar <rk7870382701@gmail.com>
2021-06-29 22:45:11 +0530

    logical error removed
    in <tr> tag extra spaces have come, just remove them and in implementing image style were incorrect to display the image url and hide and display thing

--- Superheroes-API/script.js
+++ Superheroes-API/script.js
@@ -14,24 +14,22 @@ $(document).ready(function () {
             url: `${URL}/${characterId}`,
             type: "GET",
             success: function (data) {
+                console.log(data);
 
-                if (data.size > 1) {
 
-                    console.log(data.size);
+                charName.text(data.name);
+                $("#superheroImage").attr("src", `${data.image.url}`);
+                $("#superheroImage").show(2000);
 
-                    charName.text(data.name);
-                    $("#superheroImage").attr("src", `${data.image.url}`);
-                    $("#superheroImage").attr("display", "block");
+                //====================== APPEARANCE =========================
 
-                    //====================== APPEARANCE =========================
-
-                    let _appearance = data.appearance;
-                    //console.log(_appearance);
-                    let row1 = `< tr > <td colspan="6" class="bg-dark text-primary text-center p-1"><h2>Appearance</h2></td></ > `;
-                    let row2 = `< tr >
+                let _appearance = data.appearance;
+                //console.log(_appearance);
+                let row1 = `< tr > <td colspan="6" class="bg-dark text-primary text-center p-1"><h2>Appearance</h2></td></ > `;
+                let row2 = `<tr>
                                <th>Gender</th>
                                <td>${_appearance.gender}</td>
-                            </ >
+                            </>
 
                             <tr>
                                 <th>Race</th>
@@ -59,18 +57,18 @@ $(document).ready(function () {
                              </tr>
 							`;
 
-                    result.append(row1).append(row2);
+                result.append(row1).append(row2);
 
-                    //=================== BIOGRAPHY  ================================
+                //=================== BIOGRAPHY  ================================
 
-                    let _bio = data.biography;
-                    //console.log(_bio);
-                    let row3 = `< tr > <td colspan="6" class="bg-dark text-success text-center p-1"><h2>Biography</h2></td></ > `;
+                let _bio = data.biography;
+                //console.log(_bio);
+                let row3 = `<tr> <td colspan="6" class="bg-dark text-success text-center p-1"><h2>Biography</h2></td></ > `;
 
-                    let row4 = `< tr >
+                let row4 = `<tr>
                                <th>Full Name</th>
                                <td>${_bio['full-name']}</td>
-                            </ >
+                            </>
 
                             <tr>
                                 <th>Place of Birth</th>
@@ -99,18 +97,18 @@ $(document).ready(function () {
 
 							`;
 
-                    result.append(row3).append(row4);
+                result.append(row3).append(row4);
 
-                    //==================== POWERSTATS ===========================
+                //==================== POWERSTATS ===========================
 
-                    let _power = data.powerstats;
-                    //console.log(_power);
-                    let row5 = `< tr > <td colspan="6" class="bg-dark text-danger text-center p-1"><h2>Powerstats</h2></td></ > `;
+                let _power = data.powerstats;
+                //console.log(_power);
+                let row5 = `<tr> <td colspan="6" class="bg-dark text-danger text-center p-1"><h2>Powerstats</h2></td></ > `;
 
-                    let row6 = `< tr >
+                let row6 = `<tr>
                               <th>Combat</th>
                               <td>${_power.combat}</td>
-                            </ >
+                            </tr>
                 
                             <tr>
                                <th>Intelligence</th>
@@ -139,13 +137,13 @@ $(document).ready(function () {
 
 							`;
 
-                    result.append(row5).append(row6);
+                result.append(row5).append(row6);
 
-                }
-                else {
-                    result.append("<b class='bg-warning'>Something's wrong, try again after some time </b>");
-                    result.append("<i class='bg-warning'>May be count of request limit has exceeded for the day</i>");
-                }
+
+                // else {
+                //     result.append("<b class='bg-warning'>Something's wrong, try again after some time </b>");
+                //     result.append("<i class='bg-warning'>May be count of request limit has exceeded for the day</i>");
+                // }
 
 
 
